8329b73a7488e6f70fc16715aadd394bbd93d45e
Jon Soring <soring@vedur.is>
2015-10-20 18:45:27 +0000

    Initial Gradle buils script and template java main and test files created

---
+++ src/main/java/is/ru/snidgengid/tictactoe/Main.java
@@ -0,0 +1,11 @@
+package is.ru.snidgengid.tictactoe;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+	
+	public static int getID(){
+		return 0;
+	}
+}
