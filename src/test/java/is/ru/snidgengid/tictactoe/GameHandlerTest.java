10bd56151c9caa8a781de0144e0645d4dd700ed9
soring@soring.is <soring@vedur.is>
2015-10-29 20:28:26 +0000

    Added additional test for newGame

--- src/test/java/is/ru/snidgengid/tictactoe/GameHandlerTest.java
+++ src/test/java/is/ru/snidgengid/tictactoe/GameHandlerTest.java
@@ -2,6 +2,7 @@ package is.ru.snidgengid.tictactoe;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertNotEquals;
 import static org.cthul.matchers.CthulMatchers.*;
 import org.junit.Test;
 import spark.Request;
@@ -13,7 +14,9 @@ import org.easymock.EasyMock;
 public class GameHandlerTest {
 	public static void main(String args[]) {
 		org.junit.runner.JUnitCore.main("is.ru.snidgengid.tictactoeTest");
-	}	
+	}
+
+	String BOARD_JSON_REGEX = ".*board.*:.*null,null,null.*,.*null,null,null.*,.*null,null,null.*,.*uuid.*:.*........-....-....-....-.............*,.*isWon.*:false,.*isDraw.*:false,.*whoWon.*:null,.*nextPlayer.*:.*X.*";	
 
 	@Test
 	public void testNewGameReturnsCorrectString() {
@@ -21,7 +24,19 @@ public class GameHandlerTest {
 		Response res = EasyMock.createMock(Response.class);  
 		Request req = null;
 		String response = (String)game.newGame(req, res);
-		assertThat(response,matchesPattern(".*board.*:.*null,null,null.*,.*null,null,null.*,.*null,null,null.*,.*uuid.*:.*........-....-....-....-.............*,.*isWon.*:false,.*isDraw.*:false,.*whoWon.*:null,.*nextPlayer.*:.*X.*"));
+		assertThat(response,matchesPattern(BOARD_JSON_REGEX));
+	}
+
+	@Test
+	public void testNewGameReturnsDifferentUUID() {
+		GameHandler game = new GameHandler();
+		Response res = EasyMock.createMock(Response.class);
+		Request req = null;
+		String firstResponse = (String)game.newGame(req, res);
+		String secondResponse = (String)game.newGame(req, res);
+		assertThat(firstResponse,matchesPattern(BOARD_JSON_REGEX));
+		assertThat(secondResponse,matchesPattern(BOARD_JSON_REGEX));
+		assertNotEquals(firstResponse,secondResponse);
 	}
 
 }
