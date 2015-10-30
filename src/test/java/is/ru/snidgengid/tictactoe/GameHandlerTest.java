f00d771650817a508fc8f4599f536c2da76434a8
soring@soring.is <soring@vedur.is>
2015-10-30 12:27:44 +0000

    Added test for action

--- src/test/java/is/ru/snidgengid/tictactoe/GameHandlerTest.java
+++ src/test/java/is/ru/snidgengid/tictactoe/GameHandlerTest.java
@@ -2,21 +2,26 @@ package is.ru.snidgengid.tictactoe;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.junit.Assert.assertNotEquals;
 import static org.cthul.matchers.CthulMatchers.*;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import spark.Request;
 import spark.Response;
 import org.easymock.EasyMock;  
-
-
+import static org.easymock.EasyMock.replay;
 
 public class GameHandlerTest {
+
 	public static void main(String args[]) {
 		org.junit.runner.JUnitCore.main("is.ru.snidgengid.tictactoeTest");
 	}
 
 	String BOARD_JSON_REGEX = ".*board.*:.*null,null,null.*,.*null,null,null.*,.*null,null,null.*,.*uuid.*:.*........-....-....-....-.............*,.*isWon.*:false,.*isDraw.*:false,.*whoWon.*:null,.*nextPlayer.*:.*X.*";	
+	String BOARD_FIRST_ACTION_JSON_REGEX = ".*board.*:.*null,null,null.*,.*null,.*X.*,null.*,.*null,null,null.*,.*uuid.*:.*........-....-....-....-.............*,.*isWon.*:false,.*isDraw.*:false,.*whoWon.*:null,.*nextPlayer.*:.*O.*";	
 
 	@Test
 	public void testNewGameReturnsCorrectString() {
@@ -39,4 +44,37 @@ public class GameHandlerTest {
 		assertNotEquals(firstResponse,secondResponse);
 	}
 
+	@Test
+	public void testFirstActionOnGame() {
+		try{
+		//Create new game
+		GameHandler game = new GameHandler();
+		Response res = EasyMock.createMock(Response.class);
+		Request req = EasyMock.createMock(Request.class);
+		String gameJSON = (String)game.newGame(req, res);
+		System.out.println(gameJSON);
+
+		//Create board object from response
+		ObjectMapper mapper = new ObjectMapper();
+		Board board = mapper.readValue(gameJSON,Board.class);
+
+		//Create game action object and set action
+		GameAction action = new GameAction();
+		action.setXCoord(1);
+		action.setYCoord(1);
+		action.setUUID(board.getUUID());
+
+		//Create json string of action for request
+		String actionJSON = mapper.writeValueAsString(action);
+		String actionResponse = (String)game.action(actionJSON,res);
+		System.out.println(actionResponse);
+		assertNotEquals(actionResponse,gameJSON);
+		assertThat(actionResponse,matchesPattern(BOARD_FIRST_ACTION_JSON_REGEX));
+		}
+		catch (Exception e) {
+			fail("FAIIIIL");
+		}
+	}
+
 }
+
