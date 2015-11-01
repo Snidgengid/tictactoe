041921dd542313b1bdc7dd787c4e5f1b8521f157
soring@soring.is <soring@vedur.is>
2015-11-01 00:20:08 +0000

    localhost4567 added

--- src/test/java/is/ru/snidgengid/tictactoe/SeleniumTest.java
+++ src/test/java/is/ru/snidgengid/tictactoe/SeleniumTest.java
@@ -182,7 +182,7 @@ public class SeleniumTest implements SauceOnDemandSessionIdProvider {
 
     @Test
     public void test() throws Exception {
-        driver.get("/");
+        driver.get("http://localhost:4567/");
         driver.findElement(By.id("canvas1")).click();
         driver.findElement(By.id("canvas2")).click();
         driver.findElement(By.id("canvas5")).click();
